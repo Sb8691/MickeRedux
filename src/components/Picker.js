cfaad09957ef1a96469a44051331f6298d0adc11
Micke Berg <micke.berg.dev@gmail.com>
2021-07-07 17:05:19 +0200

    first commit

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -1,16 +1,16 @@
 import React from "react";
 
 const Picker = ({ value, onChange, options }) => (
-    <span>
-        <h1>{value}</h1>
-        <select onChange={e => onChange(e.target.value)} value={value}>
+    <div className="picker">
+        <select className="select" onChange={e => onChange(e.target.value)} value={value}>
             {options.map(option => (
                 <option value={option} key={option}>
                     {option}
                 </option>
             ))}
         </select>
-    </span>
+            <h1>{value}</h1>
+    </div>
 );
 
 
--- src/containers/RedditContainer.js
+++ src/containers/RedditContainer.js
@@ -3,20 +3,28 @@ import { connect } from 'react-redux'
 import { selectReddit } from '../store/actions'
 import Picker from '../components/Picker'
 
-const RedditContainer = ({ selectedReddit, dispatch }) => {
+const RedditContainer = ({ selectedReddit, selectSubReddit, postsByReddit, dispatch, posts }) => {
 
     const handleChange = useCallback((nextReddit) => {
         // TODO: implement this method using redux-saga
+        selectSubReddit(nextReddit)
     }, [])
 
     return (
-        <div>
-            <h3>Get data from Reddit and display them here</h3>
+        <div className="wrapper">
+            <h2>Get data from Reddit and display them here</h2>
             <div>
                 <Picker value={selectedReddit} onChange={handleChange} options={['reactjs', 'frontend']} />
             </div>
             <div>
                 {/* Display reddits from selected option */}
+                {posts.map((post => (
+                    <div className="post" key={post.id}>
+                        <h3>{post.title}</h3>
+                        <p className="post-author">{post.author}</p>
+                        <p className="post-selftext">{post.selftext}</p>
+                    </div>
+                )))}
             </div>
         </div>
     );
@@ -37,4 +45,8 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps)(RedditContainer)
+const mapDispatchToProps = (dispatch) => ({
+    selectSubReddit: (reddit) => dispatch(selectReddit(reddit))
+    });
+
+export default connect(mapStateToProps, mapDispatchToProps)(RedditContainer)
